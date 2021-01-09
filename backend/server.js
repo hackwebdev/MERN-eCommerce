a28e05bdadea94181cc6f90ba11f02b16dfd13af
hackwebdev <hackwebdev@gmail.com>
2021-01-09 22:09:46 +0800

    fetching products from the database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -10,9 +11,7 @@ connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use("/api/products", productRoutes);
 
 app.get("/api/products", (req, res) => {
   res.send(products);
