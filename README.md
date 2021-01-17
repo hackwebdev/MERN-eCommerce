51a520249ed9b996b39e7a30707be9aaa18f310c
hackwebdev <hackwebdev@gmail.com>
2021-01-17 14:22:49 +0800

    bringing redux state into homescreen - usedispatch and useselector

--- README.md
+++ README.md
@@ -3,4 +3,20 @@ Create .env
     NODE_ENV = "if development or production"
     PORT = "port here"
     MONGO_URI = mongodb+srv://eckzen:<password>@cluster0.ssawl.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+Redux
+install redux devtool
+$npm i redux react-redux redux-thunk redux-devtools-extension
+store.js
+- redux
+    - createStore
+    - combineReducers
+    - applyMiddleware
+- redux-thunk
+    - thunk
+- redux-devtools-extension
+    - composeWithDevTools
+
+
+
 ```
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducers } from "./reducers/productReducers";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducers,
+});
 
-const initialState = {
-  // cart: {
-  //   cartItems: cartItemsFromStorage,
-  //   shippingAddress: shippingAddressFromStorage,
-  // },
-  // userLogin: { userInfo: userInfoFromStorage },
-};
+const initialState = {};
 
 const middleware = [thunk];
 
