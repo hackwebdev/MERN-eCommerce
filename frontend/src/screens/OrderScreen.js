f59a319bbee21e7e54ae341d0db009ceb8453cdc
hackwebdev <hackwebdev@gmail.com>
2021-02-08 21:26:13 +0800

    order pay reducer and action

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -29,7 +29,7 @@ const OrderScreen = ({ match, history }) => {
     if (!order || order._id !== orderId) {
       dispatch(getOrderDetails(orderId));
     }
-  }, [order, orderId]);
+  }, [dispatch, order, orderId]);
 
   return loading ? (
     <Loader />
