aae1876f6e11dec64a5a0531b768aaab8cbe841c
hackwebdev <hackwebdev@gmail.com>
2021-02-18 15:50:14 +0800

    product pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
@@ -40,9 +41,20 @@ const App = () => {
             component={ProductListScreen}
             exact
           />
+          <Route
+            path="/admin/productlist/:pageNumber"
+            component={ProductListScreen}
+            exact
+          />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/orderlist" component={OrderListScreen} />
           <Route path="/search/:keyword" component={HomeScreen} exact />
+          <Route path="/page/:pageNumber" component={HomeScreen} exact />
+          <Route
+            path="/search/:keyword/page/:pageNumber"
+            component={HomeScreen}
+            exact
+          />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
